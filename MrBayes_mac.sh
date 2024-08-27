9eea2fdfbe113c71603a56cc98dddcd96fbf40b6
dickgroenenberg <dickgroenenberg@users.noreply.github.com>
2024-08-27 12:13:37 +0200

    Create MrBayes_mac.sh

---
+++ MrBayes_mac.sh
@@ -0,0 +1,14 @@
+# If Homebrew is not yet installed on your system
+# open a terminal window and type:
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install MrBayes
+brew install mrbayes
+
+# Start MrBayes
+mb
+# Possibly you will encounter a shmem/mmap error - just ignore (it doesn't affect execution)
+# Show Help
+help
+# Quit MrBayes
+quit
