d7ef642a240d2fcde81569e74efe7711e42327ff
dickgroenenberg <dickgroenenberg@users.noreply.github.com>
2024-08-27 09:27:34 +0200

    Update Paup_mac.sh

--- Paup_mac.sh
+++ Paup_mac.sh
@@ -4,7 +4,7 @@ https://phylosolutions.com/paup-test/
 # Select a version for your OS:
 https://phylosolutions.com/paup-test/paup4a168_osx.gz
 
-# Files are put in the Downloads folder (default): 
+# By default the zip archive should be put in your Downloads folder: 
 # unzip the downloaded file (just click on it)
 
 # Open a terminal window, move Paup to your apps folder
